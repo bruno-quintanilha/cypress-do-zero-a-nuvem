acb5fae8f1c584b0d06528f94940599ec928b703
PNGBruninho <brunoqsousa01@gmail.com>
2025-10-06 16:06:08 -0300

    Create cypress project

---
+++ cypress/e2e/CAC-TAT.cy.js
@@ -0,0 +1,168 @@
+describe('Central de Atendimento ao Cliente TAT', () => {
+  beforeEach(() => {
+    cy.visit('./src/index.html')
+  })
+
+  it('verifica o título da aplicação', () => {
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+
+
+it('preenche os campos obrigatórios e envia o formulário', () => {
+    const longText = Cypress._.repeat('abcdefghijklmnopqrstuvwxyz', 10)
+
+    cy.get('#firstName').type('Bruno')
+    cy.get('#lastName').type('Quintanilha da Silva de Sousa')
+    cy.get('#email').type('brunoqsousa01@gmail.com')
+    cy.get('#open-text-area').type(longText, { delay:0 })
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.success').should('be.visible')
+
+  })
+
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('#firstName').type('Bruno')
+    cy.get('#lastName').type('Quintanilha da Silva de Sousa')
+    cy.get('#email').type('brunoqsousa01@teste,com')
+    cy.get('#open-text-area').type('Teste')
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.error').should('be.visible')
+  })
+
+  it('não permite valores não-numéricos no campo telefone', () => {
+    cy.get('#phone')
+    .type('abc!@#')
+    .should('have.value', '')
+  })
+
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#firstName').type('Bruno')
+    cy.get('#lastName').type('Quintanilha da Silva de Sousa')
+    cy.get('#email').type('brunoqsousa01@teste,com')
+    cy.get('#open-text-area').type('Teste')
+
+    cy.get('#phone-checkbox').check()
+
+    cy.contains('button', 'Enviar').click()
+    
+    cy.get('.error').should('be.visible')
+  })
+
+  it('preenche e limpa os campos de nome, sobrenome, email e telefone', () => {
+    cy.get('#firstName')
+    .type('Bruno')
+    .should('have.value', 'Bruno')
+    .clear()
+    .should('have.value', '')
+    cy.get('#lastName')
+    .type('Quintanilha da Silva de Sousa')
+    .should('have.value', 'Quintanilha da Silva de Sousa')
+    .clear()
+    .should('have.value', '')
+    cy.get('#email')
+    .type('brunoqsousa01@teste.com')
+    .should('have.value', 'brunoqsousa01@teste.com')
+    .clear()
+    .should('have.value', '')
+    cy.get('#phone')
+    .type('40028922')
+    .should('have.value', '40028922')
+    .clear()
+    .should('have.value', '')
+  })
+
+  it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.error').should('be.visible')
+  })
+
+  it('envia o formulário com sucesso usando um comando costumizado', () => {
+    cy.fillMandatoryFieldsAndSubmit()
+
+     cy.get('.success').should('be.visible')
+  })
+
+  it('seleciona um produto (YouTube) por seu texto', () => {
+    cy.get('#product')
+      .select('YouTube')
+      .should('have.value', 'youtube')
+  })
+
+  it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+    cy.get('#product')
+      .select('mentoria')
+      .should('have.value', 'mentoria')
+  })
+
+  it('seleciona um produto (Blog) por seu índice', () => {
+    cy.get('#product')
+      .select(1)
+      .should('have.value', 'blog')
+  })
+
+  it('marca o tipo de atendimento "Feedback"', () => {
+    cy.get('input[type="radio"][value="feedback"]')
+      .check()
+      .should('be.checked')
+  })
+
+  it('marca todos os tipos de atendimento', () => {
+     cy.get('input[type="radio"]')
+     .each(typeOfService => {
+        cy.wrap(typeOfService)
+          .check()
+          .should('be.checked')
+    })
+  })
+
+  it('marca ambos os checkboxes, depois desmarca o último', () => {
+    cy.get('input[type="checkbox"]')
+    .check()
+    .should('be.checked')
+    .last()
+    .uncheck()
+    .should('not.be.checked')
+  })
+
+  it('seleciona um arquivo da pasta fixtures', () => {
+    cy.get('#file-upload')
+    .selectFile('cypress/fixtures/example.json')
+    .should(input => {
+       expect(input[0].files[0].name).to.equal('example.json')
+    })
+  })
+
+  it('seleciona um arquivo simulando um drag-and-drop', () => {
+    cy.get('#file-upload')
+    .selectFile('cypress/fixtures/example.json', { action: 'drag-drop' })
+    .should(input => {
+      expect(input[0].files[0].name).to.equal('example.json')
+    })
+  })
+
+  it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+    cy.fixture('example.json').as('exampleFile')
+    cy.get('#file-upload')
+    .selectFile('@exampleFile')
+    .should(input => {
+      expect(input[0].files[0].name).to.equal('example.json')
+    })
+  })
+
+  it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+    cy.contains('a', 'Política de Privacidade')
+      .should('have.attr', 'href', 'privacy.html')
+      .and('have.attr', 'target', '_blank')
+  })
+
+  it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+    cy.contains('a', 'Política de Privacidade')
+    .invoke('removeAttr', 'target')
+    .click()
+
+    cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
+  })
+})
